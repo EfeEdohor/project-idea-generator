543354f06dce3f007beddd6e8d891187422fa4bf
EfeEdohor <edohor.efe@gmail.com>
2025-09-17 22:48:44 +0100

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateIdea(event) {
+  event.preventDefault();
 
+  new Typewriter("#ideas", {
+    strings: "Project Ideas",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let ideaFormElement = document.querySelector("#idea-generator-form");
+ideaFormElement.addEventListener("submit", generateIdea);
